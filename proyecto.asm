6b3cb92997b4a4a0b02c19c963b273ce12dba693
Panito115 <jpmadriz@ufm.edu>
2026-09-10 11:59:43 -0600

    cambie nombre

--- proyecto.asm
+++ proyecto.asm
@@ -28,6 +28,15 @@ ALT_J           equ 24h
 ALT_B           equ 30h
 ALT_H           equ 23h
 
+FILA_ARCHIVO    equ 0               ; limites de las tres zonas de la pantalla
+FILA_MIN        equ 1
+FILA_MAX        equ 23
+FILA_ESTADO     equ 24
+COL_MIN         equ 0
+COL_MAX         equ 79
+COLOR_ESTADO    equ 70h             ; barra de estado: fondo gris, letras negras
+ASCII_BACK      equ 08h             ; codigo ASCII de la tecla Backspace
+
 .data
 
 buf_texto   db 2000 dup(' ')        ; un caracter del documento por cada celda
@@ -40,8 +49,12 @@ texto_opcion1   db '  1. Crear archivo nuevo  $'
 texto_opcion2   db '  2. Abrir archivo existente$'
 texto_opcion3   db '  3. Salir                $'
 texto_ayuda     db 'Use flechas y Enter. Alt+X para salir.$'
-texto_edicion   db 'Pantalla de edicion pendiente.$'
-texto_regresar  db 'Presione una tecla para regresar al menu.$'
+fila_cur        db FILA_MIN             ; fila donde esta el cursor de edicion
+col_cur         db COL_MIN              ; columna donde esta el cursor de edicion
+texto_archivo   db 'Archivo: SIN NOMBRE$'
+texto_fila      db 'Fila:$'
+texto_columna   db 'Col:$'
+texto_atajo     db 'Alt+H = ayuda$'
 
 
 .code
@@ -207,32 +220,297 @@ salir_menu:
     ret
 menu_principal endp
 
-; limpia la pantalla y espera una tecla antes de regresar al menu
+; dibuja la pantalla de edicion y atiende las teclas hasta que se pida volver al menu
 pantalla_edicion proc
+    ; coloca el cursor al inicio del area de edicion y dibuja la pantalla
+    mov fila_cur, FILA_MIN
+    mov col_cur, COL_MIN
+    call dibujar_edicion
+
+ciclo_edicion:
+    ; refresca la barra de estado, coloca el cursor y espera una tecla
+    call dibujar_estado
+    mov dh, fila_cur
+    mov dl, col_cur
+    call poner_cursor
+    call leer_tecla
+
+    ; las flechas y los atajos con Alt llegan con AL igual a cero
+    cmp al, 0
+    je tecla_especial
+
+    ; Backspace borra y los demas caracteres se filtran antes de escribirlos
+    cmp al, ASCII_BACK
+    je tecla_borrar
+    call es_valido
+    cmp bh, 1
+    je tecla_normal
+    jmp ciclo_edicion
+
+tecla_normal:
+    call escribir_en_buffer
+    call avanzar_cursor
+    jmp ciclo_edicion
+
+tecla_borrar:
+    call borrar_char
+    jmp ciclo_edicion
+
+tecla_especial:
+    ; Alt+Z regresa al menu principal y las flechas mueven el cursor
+    cmp ah, ALT_Z
+    je fin_edicion
+    cmp ah, FLECHA_ARRIBA
+    je subir_fila
+    cmp ah, FLECHA_ABAJO
+    je bajar_fila
+    cmp ah, FLECHA_IZQUIERDA
+    je mover_izquierda
+    cmp ah, FLECHA_DERECHA
+    je mover_derecha
+    jmp ciclo_edicion
+
+subir_fila:
+    ; cada flecha se detiene en el borde del area de edicion
+    cmp fila_cur, FILA_MIN
+    jbe fin_subir
+    dec fila_cur
+fin_subir:
+    jmp ciclo_edicion
+
+bajar_fila:
+    cmp fila_cur, FILA_MAX
+    jae fin_bajar
+    inc fila_cur
+fin_bajar:
+    jmp ciclo_edicion
+
+mover_izquierda:
+    cmp col_cur, COL_MIN
+    jbe fin_izquierda
+    dec col_cur
+fin_izquierda:
+    jmp ciclo_edicion
+
+mover_derecha:
+    cmp col_cur, COL_MAX
+    jae fin_derecha
+    inc col_cur
+fin_derecha:
+    jmp ciclo_edicion
+
+fin_edicion:
+    ret
+pantalla_edicion endp
+
+; dibuja el nombre del archivo, el documento y la barra de estado
+dibujar_edicion proc
     push bx
     push dx
     push si
 
-    ; muestra una pantalla temporal hasta implementar la edicion real
+    ; limpia la pantalla y pinta el contenido del documento
     mov bl, COLOR_FONDO
     call limpiar_pantalla
-    mov dh, 10
-    mov dl, 25
-    mov bl, COLOR_MARCO
-    lea si, texto_edicion
-    call escribir_texto
+    call pintar_buffer
 
-    mov dh, 12
-    mov dl, 18
-    lea si, texto_regresar
+    ; escribe el nombre del archivo en la fila reservada de arriba
+    mov dh, FILA_ARCHIVO
+    mov dl, 1
+    mov bl, COLOR_MARCO
+    lea si, texto_archivo
     call escribir_texto
-    call leer_tecla
+    call dibujar_estado
 
     pop si
     pop dx
     pop bx
     ret
-pantalla_edicion endp
+dibujar_edicion endp
+
+; pinta la barra de estado con la fila y la columna donde esta el cursor
+dibujar_estado proc
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+
+    ; pinta las 80 columnas de la ultima fila con el color de la barra
+    mov cx, 80
+    mov dh, FILA_ESTADO
+    mov dl, COL_MIN
+    mov al, ' '
+    mov bl, COLOR_ESTADO
+
+estado_ciclo:
+    call escribir_char
+    inc dl
+    loop estado_ciclo
+
+    ; escribe la fila actual del cursor
+    mov dl, 1
+    lea si, texto_fila
+    call escribir_texto
+    mov dl, 7
+    mov al, fila_cur
+    call mostrar_numero
+
+    ; escribe la columna actual del cursor
+    mov dl, 11
+    lea si, texto_columna
+    call escribir_texto
+    mov dl, 16
+    mov al, col_cur
+    call mostrar_numero
+
+    ; recuerda el atajo de la ayuda
+    mov dl, 60
+    lea si, texto_atajo
+    call escribir_texto
+
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+dibujar_estado endp
+
+; escribe el numero de AL en dos digitos, en la fila DH y la columna DL con el color BL
+mostrar_numero proc
+    push ax
+    push bx
+    push dx
+
+    ; separa las decenas de las unidades y las vuelve caracteres
+    mov ah, 0
+    mov bh, 10
+    div bh
+    add al, '0'
+    add ah, '0'
+
+    ; dibuja primero las decenas y luego las unidades
+    call escribir_char
+    inc dl
+    mov al, ah
+    call escribir_char
+
+    pop dx
+    pop bx
+    pop ax
+    ret
+mostrar_numero endp
+
+; revisa el caracter de AL y devuelve BH en 1 cuando se puede escribir
+es_valido proc
+    ; los cuatro signos permitidos se revisan uno por uno
+    mov bh, 1
+    cmp al, ' '
+    je valido_fin
+    cmp al, ','
+    je valido_fin
+    cmp al, '.'
+    je valido_fin
+    cmp al, ':'
+    je valido_fin
+
+    ; los numeros y las letras se revisan por rangos
+    cmp al, '0'
+    jb valido_no
+    cmp al, '9'
+    jbe valido_fin
+    cmp al, 'A'
+    jb valido_no
+    cmp al, 'Z'
+    jbe valido_fin
+    cmp al, 'a'
+    jb valido_no
+    cmp al, 'z'
+    jbe valido_fin
+
+valido_no:
+    mov bh, 0
+
+valido_fin:
+    ret
+es_valido endp
+
+; guarda el caracter de AL en la posicion del cursor y lo dibuja en la pantalla
+escribir_en_buffer proc
+    push ax
+    push bx
+    push dx
+
+    ; calcula la posicion dentro del buffer: fila * 80 + columna
+    push ax
+    mov al, fila_cur
+    mov bl, 80
+    mul bl
+    mov bx, 0
+    mov bl, col_cur
+    add bx, ax
+    pop ax
+
+    ; guarda el caracter con su color y lo dibuja en la pantalla
+    mov buf_texto[bx], al
+    mov buf_color[bx], COLOR_DEF
+    mov dh, fila_cur
+    mov dl, col_cur
+    mov bl, COLOR_DEF
+    call escribir_char
+
+    pop dx
+    pop bx
+    pop ax
+    ret
+escribir_en_buffer endp
+
+; mueve el cursor una columna a la derecha y baja de fila al pasar la ultima columna
+avanzar_cursor proc
+    ; mientras no sea la ultima columna solo avanza a la derecha
+    cmp col_cur, COL_MAX
+    jb avanzar_columna
+
+    ; al pasar la ultima columna regresa al inicio de la fila de abajo
+    mov col_cur, COL_MIN
+    cmp fila_cur, FILA_MAX
+    jae avanzar_fin
+    inc fila_cur
+    jmp avanzar_fin
+
+avanzar_columna:
+    inc col_cur
+
+avanzar_fin:
+    ret
+avanzar_cursor endp
+
+; borra el caracter que esta antes del cursor y deja el cursor en ese lugar
+borrar_char proc
+    push ax
+
+    ; retrocede una columna, o sube al final de la fila anterior
+    cmp col_cur, COL_MIN
+    ja borrar_izquierda
+    cmp fila_cur, FILA_MIN
+    jbe borrar_fin
+    dec fila_cur
+    mov col_cur, COL_MAX
+    jmp borrar_espacio
+
+borrar_izquierda:
+    dec col_cur
+
+borrar_espacio:
+    ; deja un espacio en el buffer y en la pantalla
+    mov al, ' '
+    call escribir_en_buffer
+
+borrar_fin:
+    pop ax
+    ret
+borrar_char endp
 
 ; llena las 2000 celdas de la pantalla con espacios y el color que viene en BL
 limpiar_pantalla proc
